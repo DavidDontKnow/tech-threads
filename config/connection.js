6eedebade5dd05e3794ed85b6341812b9e6a0b15
DavidDontKnow <DavidDontKnow02@gmail.com>
2023-03-31 23:11:20 -0700

    Helper util and fixed file name

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-const sequelize = require('../config/connection');
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
-        process.env.DB_PW,
+        process.env.DB_PASSWORD,
         {
             host: 'localhost',
             dialect: 'mysql',
